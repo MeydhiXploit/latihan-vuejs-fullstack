b3bfb665f4b0cb9da145b9352a85b0de9c77e50a
MeydhiXploit <meydhimeydhi@gmail.com>
2024-08-05 00:39:02 -0700

    i added app js and index

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-
 var product = {
     "name": "Meydhi Ari Nugroho",
     "description": "You'll be the most environmentally conscious",
     "price": "99",
     "image": "https://www.pngplay.com/wp-content/uploads/12/Big-Hero-6-PNG-HD-Photos.png"
 };
-
 var app = new Vue({
     el:'#app',
     data:product
