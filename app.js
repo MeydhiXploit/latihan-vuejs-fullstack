fb1dc854df535ad6e7dd2ff1ef59d6f0d33913ac
MeydhiXploit <meydhimeydhi@gmail.com>
2024-08-05 00:13:15 -0700

    i added app js and index

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var product = {
     "name": "Meydhi Ari Nugroho",
     "description": "You'll be the most environmentally conscious",
     "price": "99",
-    "image": "https://www.google.com/imgres?q=image%20world%20skill&imgurl=https%3A%2F%2Fcdn.antaranews.com%2Fcache%2F1200x800%2F2019%2F08%2F29%2FIMG-20190829-WA0006.jpg&imgrefurl=https%3A%2F%2Fwww.antaranews.com%2Fberita%2F1035070%2Fsiswa-smk-boyong-15-medali-pada-world-skill-competition&docid=5G6_KTST2F-2iM&tbnid=Lj-1NxMmZPLt3M&vet=12ahUKEwjc2s6M29yHAxXCwTgGHffSB6AQM3oECB0QAA..i&w=1200&h=800&hcb=2&ved=2ahUKEwjc2s6M29yHAxXCwTgGHffSB6AQM3oECB0QAA"
+    "image": "https://www.pngplay.com/wp-content/uploads/12/Big-Hero-6-PNG-HD-Photos.png"
 };
 
 var app = new Vue({
