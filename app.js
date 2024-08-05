af714ec5a401c4b9a01947f2570d01c26d80fac7
MeydhiXploit <meydhimeydhi@gmail.com>
2024-08-04 18:39:56 -0700

    i added product as data

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+
+var product = {
+    "name": "Meydhi Ari Nugroho",
+    "description": "You'll be the most environmentally conscious",
+    "price": "99",
+    "image": "https://www.google.com/imgres?q=image%20world%20skill&imgurl=https%3A%2F%2Fcdn.antaranews.com%2Fcache%2F1200x800%2F2019%2F08%2F29%2FIMG-20190829-WA0006.jpg&imgrefurl=https%3A%2F%2Fwww.antaranews.com%2Fberita%2F1035070%2Fsiswa-smk-boyong-15-medali-pada-world-skill-competition&docid=5G6_KTST2F-2iM&tbnid=Lj-1NxMmZPLt3M&vet=12ahUKEwjc2s6M29yHAxXCwTgGHffSB6AQM3oECB0QAA..i&w=1200&h=800&hcb=2&ved=2ahUKEwjc2s6M29yHAxXCwTgGHffSB6AQM3oECB0QAA"
+};
+
 var app = new Vue({
     el:'#app',
-    data:{
-        name:'Meydhi Ari Nugroho'
-    }
+    data:product
 });
