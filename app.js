ba5ccfb6311966898902d731c8ed4b1f830fc260
MeydhiXploit <meydhimeydhi@gmail.com>
2024-08-05 19:02:12 -0700

    i added app and index

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-var product = {
-    "name": "Meydhi Ari Nugroho",
-    "description": "You'll be the most environmentally conscious",
-    "price": "99",
-    "image": "https://www.pngplay.com/wp-content/uploads/12/Big-Hero-6-PNG-HD-Photos.png"
+var dataProduct = {
+    products: [{
+            "id": "429",
+            "name": "Strawberry Mineral Water",
+            "description": "An 8-ounce serving of our refreshing",
+            "price": "2.80",
+            "image_title": "mineral-water-strawberry_600px",
+            "image": "https://www.coldwear.com.sg/image/cache/product/product-72679/10115__Joel__1666174307_MWS020037_BLU_2-272x408.jpg",
+        },
+        {
+            "id": "440",
+            "name": "Adult Mid Length Goose Down Jacket",
+            "description": "An 10-ounce serving of our refreshing",
+            "price": "3.00",
+            "image_title": "adult-mid_600px",
+            "image": "https://www.coldwear.com.sg/image/cache/product/product-76165/10115__Joel__1722305075_10115__Joel__1571217725_LDJ019117_BLU2-opt_1-272x408.jpg",
+        },
+        {
+            "id": "450",
+            "name": "man",
+            "description": "An 11-ounce serving of our refreshing",
+            "price": "3.80",
+            "image_title": "cool",
+            "image": "https://www.coldwear.com.sg/image/cache/product/product-72679/10115__Joel__1666174307_MWS020037_BLU_2-272x408.jpg",
+        },
+
+    ]
 };
 var app = new Vue({
-    el:'#app',
-    data:product
+    el: '#app',
+    data: dataProduct
 });
