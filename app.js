ccc201cda2485b6b2243eb8fb34d9ee3439b1125
MeydhiXploit <meydhimeydhi@gmail.com>
2024-08-16 01:02:39 -0700

    i added app js mounted lifecycle hook

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var app = new Vue({
         products:null
     },
     mounted: function(){
+        //lifecycle hook vue.js yang dapat di panggil setelah instance vue sepenuhnya di buat
+        //dan elemen DOM terkait telah di masukkan kedalam halaman.
         fetch('https://hplussport.com/api/products/order/price')
         .then(response => response.json()
         .then(data => {
