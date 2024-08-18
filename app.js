9d1c65f113a97ee23725d93fdfc400a1bb719655
MeydhiXploit <meydhimeydhi@gmail.com>
2024-08-17 20:31:45 -0700

    i commit app js

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ var app = new Vue({
     el: '#app',
     data: {
         maximum:50,
-        products:null
+        products: null
     },
     mounted: function(){
-        //lifecycle hook vue.js yang dapat di panggil setelah instance vue sepenuhnya di buat
-        //dan elemen DOM terkait telah di masukkan kedalam halaman.
         fetch('https://hplussport.com/api/products/order/price')
         .then(response => response.json()
         .then(data => {
@@ -14,4 +12,4 @@ var app = new Vue({
         })
     )
     }
-});
+})
