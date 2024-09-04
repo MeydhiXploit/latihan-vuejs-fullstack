0a5d90599cea1cd4acbe3b19467ef46ccd13fe05
MeydhiXploit <meydhimeydhi@gmail.com>
2024-09-04 05:25:41 -0700

    feat: app 3

---
+++ app3.js
@@ -0,0 +1,19 @@
+var app = new Vue({
+    el:'#app',
+    data:data
+})
+
+var data = {
+    products:[
+        {
+            "id": "429",
+            "name": "Strawbery Mineral Water",
+            "description": "An 8-ounce serving of our refreshing H+ Sport",
+            "price": "2.80",
+            "image_title": "mineral-water-strawberyy_600px",
+            "image": ""
+
+
+        }
+    ]
+}
