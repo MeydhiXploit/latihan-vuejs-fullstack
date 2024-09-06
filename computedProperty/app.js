403b3b1786a2b6e2e7b6640338fe0e5897abdcd3
MeydhiXploit <meydhimeydhi@gmail.com>
2024-09-05 19:22:28 -0700

    feat: slugtext

--- computedProperty/app.js
+++ computedProperty/app.js
@@ -2,5 +2,11 @@ var app = new Vue({
     el: '#app',
     data: {
         slugText: 'The Quick #C*sdf Brown 29'
+    },
+    computed:{
+        slugtize: function(){
+            // fungsi kode ini menampilkan nilai balik dari slugText
+            return this.slugText
+        }
     }
 })
